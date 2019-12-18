a5fbe29f5093a3ca8e051d31dcaf32a97647e0cd
王澍 <wangshu@ws>
2019-12-18 10:25:31 +0800

    second commit

--- list.c
+++ list.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 LinkList init(){
     /*List _list = (List)malloc(sizeof(struct list) * length);
     _list->prev = NULL;
@@ -16,15 +17,11 @@ LinkList init(){
     LinkList  link_List = (LinkList)malloc(sizeof(linkList));
     link_List->length = 0;
     link_List->size = 0;
-    link_List->list = NULL;
     link_List->tail = NULL;
     link_List->head = NULL;
     return link_List;
 }
 int add(LinkList  _linkList,Node _node){
-    Node node = _linkList->list;
-    node->tail = _node;
-    _node->prev = node;
     _linkList->length++;
     _linkList->tail = _node;
     return 1;
--- list.h
+++ list.h
@@ -5,6 +5,14 @@
 #ifndef PROJECT_LIST_H
 #define PROJECT_LIST_H
 
+// 获取"MEMBER成员"在"结构体TYPE"中的位置偏移
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+
+// 根据"结构体(type)变量"中的"域成员变量(member)的指针(ptr)"来获取指向整个结构体变量的指针
+#define container_of(ptr, type, member) ({          \
+      const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+      (type *)( (char *)__mptr - offsetof(type,member) );})
+
 typedef  struct list_node{ //链表
     struct node * prev;
     struct node * tail;
@@ -13,12 +21,10 @@ typedef  struct list_node{ //链表
 typedef struct node* Node;
 
 typedef struct linkList{
-    Node list;
     int size;
     int length;
     Node tail;  //链表中的头节点
     Node head;  //链表中的尾节点
-    void * data;
 }linkList;
 typedef linkList* LinkList;
 
